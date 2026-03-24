51ad15f3811cb18ad3c83816c8b52c940fe8a28e
Texturas Dev <dev@texturas.app>
2026-03-24 23:21:49 +0000

    Revert AGP to 8.0.2 and SDK to 33 for stability

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
-    id("com.android.application") version "8.2.0" apply false
+    id("com.android.application") version "8.0.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("kotlin-kapt") apply false
+    id("com.google.dagger.hilt.android") version "2.48" apply false
 }
 
 tasks.register<Delete>("clean") {
